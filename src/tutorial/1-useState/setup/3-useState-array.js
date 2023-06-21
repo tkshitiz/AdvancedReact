bdb6285a85233c308e5db8a24970d50703691aa5
kstha022 <kshitiz.thapa@javra.com>
2023-06-21 17:59:10 +0545

    UseState-example

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,26 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [Peoples, setPeople] = React.useState(data)
+
+  const removeItems =(id) =>{
+    let newPeople=Peoples.filter((people) => people.id !== id)
+    setPeople(newPeople)
+  }
+  return<>
+    {
+     Peoples.map((people) => {
+     const {id, name} = people  //array destructuring in js
+     return (
+      <div key= {id} className='item'>
+            <h4>{name}</h4> 
+      <button onClick={()=>removeItems(id)} >remove</button> 
+      </div>
+     )
+     })
+    }
+      <button className='btn' onClick={() => setPeople([])}>clear items </button>  
+    </>
 };
 
 export default UseStateArray;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+// const [person, setPerson] = useState({
+//   name: 'kshitiz thapa',
+//   age:69,
+//   message : 'random title'
+// })
+
+const [name , setName]= useState('kshitiz thapa')
+const [age , setAge]= useState(69)
+const [message , setMessage]= useState('default value')
+
+
+// const changeMessage =() =>{
+//   setMessage('hello world')
+  // if(person.message === 'random title')
+  // {
+  //    setPerson({...person, message:'change vayo'})
+  // }
+  // else{
+  //   setPerson({...person, message: 'random title'})
+  // }
+  
+// }
+  return <>
+  <h3>{name}</h3>
+   <h3>{age}</h3>
+    <h3>{message}</h3>
+    <button className='btn' onClick={() =>{setMessage('hellow world')}}>change</button>
+  </>;
+ 
 };
+
 
 export default UseStateObject;
